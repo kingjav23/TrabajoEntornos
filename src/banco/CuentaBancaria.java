6db9a473e5da3f8c64286ce6a62f4a6ee978c7fe
Javier <Javier@PC-King>
2019-06-02 20:09:53 +0200

    Primera version de la aplicacion

--- src/banco/CuentaBancaria.java
+++ src/banco/CuentaBancaria.java
@@ -10,5 +10,61 @@ package banco;
  * @author Javier
  */
 public class CuentaBancaria {
+      String nombrecb;
+    int saldocb;
+    String idcb;
+    Cliente clientecb;
     
+    CuentaBancaria(){
+        
+    }
+    
+    CuentaBancaria(String nombrecb, int saldocb, String idcb, Cliente clientecb){
+        this.nombrecb = nombrecb;
+        this.saldocb = saldocb;
+        this.idcb = idcb;
+        this.clientecb = clientecb;
+    }
+
+    public String getNombrecb() {
+        return nombrecb;
+    }
+
+    public void setNombrecb(String nombrecb) {
+        this.nombrecb = nombrecb;
+    }
+
+    public int getSaldocb() {
+        return saldocb;
+    }
+
+    public void setSaldocb(int saldocb) {
+        this.saldocb = saldocb;
+    }
+
+    public String getIdcb() {
+        return idcb;
+    }
+
+    public void setIdcb(String idcb) {
+        this.idcb = idcb;
+    }
+
+    public Cliente getClientecb() {
+        return clientecb;
+    }
+
+    public void setClientecb(Cliente clientecb) {
+        this.clientecb = clientecb;
+    }
+    
+    public boolean ingresar(int ingreso){
+        setSaldocb(getSaldocb() + ingreso);
+        return true;
+    }
+    
+    public boolean retirar(int retirado){
+        setSaldocb(getSaldocb() - retirado);
+        return true;
+    }  
 }
